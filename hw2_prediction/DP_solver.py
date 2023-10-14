8f50c4627b2463dc4286e7890e7bee4b5c67fef8
EdwardLeeMacau <b05901119@ntu.edu.tw>
2023-10-14 11:33:36 +0800

    Impl Model-Free Control
    Squashed commit of the following:
    
    commit bc5286983c3d083d4dded32c2ec4719fdf7e2687
    Author: EdwardLeeMacau <b05901119@ntu.edu.tw>
    Date:   Sat Oct 14 11:30:50 2023 +0800
    
        Finish TD-lambda estimation
    
    commit fe7f87bb6885037f92c8e8fa42fd81df3e9aa89c
    Author: EdwardLeeMacau <b05901119@ntu.edu.tw>
    Date:   Sat Oct 14 10:46:00 2023 +0800
    
        Finish TD(0) estimation
    
    commit d2f660632563cba4f60ae0d61668cfa5b630af32
    Author: EdwardLeeMacau <b05901119@ntu.edu.tw>
    Date:   Sat Oct 14 10:40:24 2023 +0800
    
        Finish TD(0) estimation
    
    commit 9301118e0bac3f804f98ec5c2fb9c3cc95127e94
    Author: EdwardLeeMacau <b05901119@ntu.edu.tw>
    Date:   Sat Oct 14 10:32:01 2023 +0800
    
        Finish MC estimation

--- hw2_prediction/DP_solver.py
+++ hw2_prediction/DP_solver.py
@@ -2,8 +2,13 @@ import numpy as np
 import json
 from collections import defaultdict
 
+from typing import List, Union, Tuple
 from gridworld import GridWorld
 
+import pysnooper
+
+# Type hinting
+State, Reward = int, float
 
 class DynamicProgramming:
     """Base class for dynamic programming algorithms"""
@@ -36,13 +41,37 @@ class MonteCarloPrediction(DynamicProgramming):
         """
         super().__init__(grid_world, discount_factor)
 
+        # Initialize the sampled returns table
+        self.returns = defaultdict(list)
+
     def run(self) -> None:
         """Run the algorithm until self.grid_world.check() == False"""
         # TODO: Update self.values with first-visit Monte-Carlo method
         current_state = self.grid_world.reset()
+        episodes: List[Tuple[State, Reward]] = []
         while self.grid_world.check():
             next_state, reward, done = self.grid_world.step()
-            continue
+
+            # Push S_{t}, R_{t+1} into episodes
+            episodes.append((current_state, reward))
+            current_state = next_state
+
+            # Collect an episode until done-flag is True.
+            if not done:
+                continue
+
+            # Then, do the first-visit MC update for each state in the episode.
+            G, states = 0, [s for s, _ in episodes]
+            for s, r in episodes[::-1]:
+                G = self.discount_factor * G + r
+
+                states.pop()
+                if s not in states:
+                    self.returns[s].append(G)
+                    self.values[s] = np.mean(self.returns[s])
+
+            # After updated the state-value table V(s), reset the episode.
+            episodes = []
 
 
 class TDPrediction(DynamicProgramming):
@@ -64,7 +93,15 @@ class TDPrediction(DynamicProgramming):
         current_state = self.grid_world.reset()
         while self.grid_world.check():
             next_state, reward, done = self.grid_world.step()
-            continue
+
+            current_value = self.values[current_state]
+            next_value    = (self.values[next_state] if not done else 0)
+
+            # Update the state-value table V(s)
+            self.values[current_state] += \
+                self.lr * (reward + self.discount_factor * next_value - current_value)
+
+            current_state = next_state
 
 
 class NstepTDPrediction(DynamicProgramming):
@@ -81,10 +118,39 @@ class NstepTDPrediction(DynamicProgramming):
         self.lr     = learning_rate
         self.n      = num_step
 
+    # @pysnooper.snoop()
     def run(self) -> None:
         """Run the algorithm until self.grid_world.check() == False"""
         # TODO: Update self.values with N-step TD Algorithm
         current_state = self.grid_world.reset()
+        episodes: List[Tuple[State, Reward]] = []
+        t, T = 0, float('inf')
         while self.grid_world.check():
-            next_state, reward, done = self.grid_world.step()
-            continue
+            if t < T:
+                next_state, reward, done = self.grid_world.step()
+
+                # Push S_{t}, R_{t+1} into buffer
+                episodes.append((current_state, reward))
+                current_state = next_state
+
+                # if terminal state, set T = t + 1. Otherwise, set T = inf
+                T = t + 1 if done else T
+
+            # Do the TD(lambda) update when tau is non-negative
+            if (tau := t - self.n + 1) >= 0:
+                G = sum([(self.discount_factor ** i) * r for i, (_, r) in enumerate(episodes[tau:])])
+
+                if tau + self.n < T:
+                    G += (self.discount_factor ** self.n) * self.values[next_state]
+
+                # Update the state-value table V(s)
+                self.values[episodes[tau][0]] += self.lr * (G - self.values[episodes[tau][0]])
+
+            # Update the time step
+            t += 1
+
+            # If the episode is done, reset the episode.
+            if tau == T - 1:
+                episodes.clear()
+                t, T = 0, float('inf')
+
